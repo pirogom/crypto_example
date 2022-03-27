4b510e7e42836f1e2339da27ee79925e6a44a40c
Pirogom <mop.pirogom@gmail.com>
2022-03-27 16:08:34 +0900

    add ed25519 test code

--- ed25519_test.go
+++ ed25519_test.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"os"
 	"testing"
 )
 
@@ -41,4 +42,50 @@ func TestEcdsa1(t *testing.T) {
 	} else {
 		fmt.Println("Verify Failed")
 	}
+}
+
+func TestEcdsa2(t *testing.T) {
+	const (
+		privateKeyFile = "ed25519_private.pem"
+		publicKeyFile  = "ed25519_public.pem"
+	)
+
+	eh := ECDSAHelper{}
+
+	if err := eh.Generate(); err != nil {
+		panic(err)
+	}
+
+	if err := eh.PrivateKeyToFile(privateKeyFile); err != nil {
+		panic(err)
+	}
+
+	if err := eh.PublicKeyToFile(publicKeyFile); err != nil {
+		panic(err)
+	}
+
+	msg := "ed25519 test msg 2"
+
+	signedBuf, err := eh.Sign(msg)
+
+	if err != nil {
+		panic(err)
+	}
+
+	eh2 := ECDSAHelper{}
+
+	if err := eh2.PrivateKeyFromFile(privateKeyFile); err != nil {
+		panic(err)
+	}
+	if err := eh2.PublicKeyFromFile(publicKeyFile); err != nil {
+		panic(err)
+	}
+
+	if eh2.Verify(msg, signedBuf) {
+		fmt.Println("Verify OK")
+	} else {
+		fmt.Println("Verify Failed")
+	}
+	os.Remove(privateKeyFile)
+	os.Remove(publicKeyFile)
 }
